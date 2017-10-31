ada4e1bb38b801c09cccaa6f06334d78ae9a97bb
donrandy <donrandy@gmail.com>
2017-10-31 18:34:36 +0100

    Update upgplayer.php

--- www/upgplayer.php
+++ www/upgplayer.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>UPG Media Player</title>
-<link href="http://vjs.zencdn.net/5.9.2/video-js.css" rel="stylesheet">
+<title>Ramalin Media Player</title>
+<link href="https://vjs.zencdn.net/5.18.4/video-js.css" rel="stylesheet">
 <style>
 .video-js {
 	padding-top: 56.25%
@@ -21,15 +21,14 @@ $m3u8= $_GET["m3u8"]; $poster= $_GET["poster"]; $title= $_GET["channel"]; $rtmp 
 <div class="wrapper">
   <div class="videocontent">
     <video id=example-video class="video-js vjs-default-skin vjs-fullscreen" preload=none  poster="<?php echo $poster;?>" width=auto height=auto  autoplay=true>
-    <source src="<?php echo $rtmp; ?>" type="rtmp/mp4">
       <source src="<?php echo $m3u8;?>" type="application/x-mpegURL">
+    <source src="<?php echo $rtmp; ?>" type="rtmp/mp4">
     </video>
   </div>
 </div>
 
 <script src="//api.peer5.com/peer5.js?id=hd7h1bwrmd47jjsan6s0"></script>
 <script src="//api.peer5.com/peer5.videojs5.plugin.js"></script>
-<link href="https://vjs.zencdn.net/5.18.4/video-js.css" rel="stylesheet">
 <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 <script> var player = videojs('example-video');</script>
 <script>
